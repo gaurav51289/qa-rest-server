9903fdfa6bc69c4954d62811cd6306a1af6a7611
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-11-28 13:22:32 -0800

    removes categorizer import

--- app/__init__.py
+++ app/__init__.py
@@ -2,26 +2,32 @@ from flask_api import FlaskAPI
 from flask import request, jsonify
 from flask_cors import CORS
 
-from categorizer import getCategories
-
-
 def create_app():
     app = FlaskAPI(__name__, instance_relative_config=True)
 
     CORS(app)
 
-    @app.route('/categorize/', methods=['POST'])
+    @app.route('/qa/', methods=['POST'])
     def ask():
         if request.method == "POST":
             question = str(request.data.get('question'))
-            if question:
+            candidate = str(request.data.get('candidate'))
 
-                que_cats = getCategories(question)
+            #TODO: answer id in the request probably
+
+            if question and candidate:
+
+                #TODO: calculate probability
+
+                probability = 0.456
 
                 response = jsonify({
-                    'categories': que_cats
+                    'answerid': '100001',
+                    'probability': probability
                 })
+
                 response.status_code = 200
+
                 return response
 
     return app
